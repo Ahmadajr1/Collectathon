c41e74d9b0322055b2332b5d70ac2b9d617eccd3
Ahmed AlRamadhan <ahmadajr1@gmail.com>
2022-08-11 23:19:44 +0300

    Implemented Instructions Scene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private string firstLevel;
+    [SerializeField] private string firstLevelScene;
+    [SerializeField] private string instructionsScene;
+    [SerializeField] private string MainMenuScene;
+
+
 
     public void StartNewGame()
     {
-        SceneManager.LoadScene(firstLevel);
+        SceneManager.LoadScene(firstLevelScene);
+    }
+
+    public void OpenInstructionsMenu()
+    {
+        SceneManager.LoadScene(instructionsScene);
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(MainMenuScene);
     }
 
     public void QuitGame()
